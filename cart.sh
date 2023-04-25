5e92dce5c40270bca5bca09081cd318dad35293d
saichakradhar <saichakradhar1427@gmail.com>
2023-04-25 17:33:19 +0530

    cart service

--- cart.sh
+++ cart.sh
@@ -3,29 +3,27 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 echo -e "\e[36m>>>>>>>>>>>>>>Installing Node js repos<<<<<<<<<<<<<<<<<\e[0m"
 yum install nodejs -y
-echo -e "\e[36m>>>>>>>>>>>>>>Adding user<<<<<<<<<<<<<<<<<\e[0m"
+
+echo -e "\e[36m>>>>>>>>>>>>>>Add Application user<<<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo -e "\e[36m>>>>>>>>>>>>>>Create Application Directory<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>Create Application directory<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /app
 mkdir /app
 
-echo -e "\e[36m>>>>>>>>>>>>>>Configuring roboshop artifacts<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>Download Application content<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 
-
-echo -e "\e[36m>>>>>>>>>>>>>>Unzip user.zip file<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>unzip Application content<<<<<<<<<<<<<<<<<\e[0m"
 unzip /tmp/cart.zip
+cd /app
 
-echo -e "\e[36m>>>>>>>>>>>>>>Install nodejs Dependencies<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>Install Node js<<<<<<<<<<<<<<<<<\e[0m"
 npm install
+cp   /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
 
-echo -e "\e[36m>>>>>>>>>>>>>>copying files <<<<<<<<<<<<<<<<<\e[0m"
-cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
-
+echo -e "\e[36m>>>>>>>>>>>>>>Start Catalogue Service<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
-
-echo -e "\e[36m>>>>>>>>>>>>>>Restart User<<<<<<<<<<<<<<<<<\e[0m"
-systemctl enable cart
-systemctl start cart
+systemctl enable catalogue
+systemctl start catalogue
