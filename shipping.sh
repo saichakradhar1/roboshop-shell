576bc69371b39aa67b0d8f0c92b079f5389b5162
saichakradhar <saichakradhar1427@gmail.com>
2023-04-25 16:41:57 +0530

    frontend script and config

--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,33 @@
+echo -e "\e[36m>>>>>>>>>>>>>>Installing maven<<<<<<<<<<<<<<<<<\e[0m"
 yum install maven -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>Create App user<<<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>>>>>>create App directory<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>>>>>>Download App content<<<<<<<<<<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>>>>>>Download maven dependencies<<<<<<<<<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+
+echo -e "\e[36m>>>>>>>>>>>>>>Installing my sql<<<<<<<<<<<<<<<<<\e[0m"
+yum install mysql -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>Load schema<<<<<<<<<<<<<<<<<\e[0m"
+mysql -h mysql-dev.rdevops72.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>>>>>>>>>>>Setup systemd service <<<<<<<<<<<<<<<<<\e[0m"
+cp  /root/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[36m>>>>>>>>>>>>>>Restart shipping<<<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
-
-yum install mysql -y
-mysql -h mysql-dev.rdevops72.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl restart shipping
