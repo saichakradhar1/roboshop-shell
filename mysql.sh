7c1c44fa97cc3453e5b759385e78421144b8b9f4
saichakradhar <saichakradhar1427@gmail.com>
2023-04-25 14:00:54 +0530

    frontend script and config

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
