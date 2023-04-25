db9e13335ebe316e2c5ee4f0869f727c465f3a99
saichakradhar <saichakradhar1427@gmail.com>
2023-04-25 16:52:38 +0530

    frontend script and config

--- cart.sh
+++ cart.sh
@@ -1,13 +1,30 @@
+echo -e "\e[36m>>>>>>>>>>>>>>Configuring Node js repos<<<<<<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-sudo yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>Installing Node js repos<<<<<<<<<<<<<<<<<\e[0m"
+yum install nodejs -y
+echo -e "\e[36m>>>>>>>>>>>>>>Adding user<<<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>>>>>>Create Application Directory<<<<<<<<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+
+echo -e "\e[36m>>>>>>>>>>>>>>Configuring roboshop artifacts<<<<<<<<<<<<<<<<<\e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
-unzip /tmp/cart.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>>>>>>>Unzip user.zip file<<<<<<<<<<<<<<<<<\e[0m"
+unzip /tmp/user.zip
+
+echo -e "\e[36m>>>>>>>>>>>>>>Install nodejs Dependencies<<<<<<<<<<<<<<<<<\e[0m"
 npm install
+
+echo -e "\e[36m>>>>>>>>>>>>>>copying files <<<<<<<<<<<<<<<<<\e[0m"
 cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
 systemctl daemon-reload
+
+echo -e "\e[36m>>>>>>>>>>>>>>Restart User<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable cart
 systemctl start cart
